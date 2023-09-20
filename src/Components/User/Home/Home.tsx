ddb671a40f201cf848b693a7863b52fda6bc3474
Muhammed Shahinsha.c <muhammedshahinsha2442@gmail.com>
2023-09-20 09:57:04 +0530

    Change to DevColabs-Client/ /

--- src/Components/User/Home/Home.tsx
+++ src/Components/User/Home/Home.tsx
@@ -203,7 +203,7 @@ function HomePage() {
         setliked(true)
         dispatch(updateUser({}));
         localStorage.removeItem("user");
-        Navigate('/');
+        Navigate('/DevColabs-Client/');
       } else {
         setliked(false);
         const response = await api.post(`/Postslike/${PostId}`);
@@ -434,13 +434,13 @@ function HomePage() {
                           <ul>
                             {(!clickedHashtag && selectCategory === 'Latest' || selectCategory === 'Recommended') ? (
                               <li className={`flex cursor-pointer relative items-center h-12 space-x-2 `}>
-                                <h1 onClick={() => Navigate('/')} className="font-bold ml-3 p-3 relative text-xl">Home</h1>
+                                <h1 onClick={() => Navigate('/DevColabs-Client/')} className="font-bold ml-3 p-3 relative text-xl">Home</h1>
                               </li>
                             ) : (
                               <>
                                 <div className="ml-8 mb-2">
                                   <li className={`flex cursor-pointer relative items-center  mt-3   h-12 space-x-2 `}>
-                                    <h1 onClick={() => Navigate('/')} className="font-semibold ml-3   relative text-3xl">{Tag?.HashtagName}</h1>
+                                    <h1 onClick={() => Navigate('/DevColabs-Client/')} className="font-semibold ml-3   relative text-3xl">{Tag?.HashtagName}</h1>
                                   </li>
                                   <div className="flex sm:ml-5 ml-11 relative">
                                     <p className="font-medium">{Tag?.CountPost}</p>
@@ -494,7 +494,7 @@ function HomePage() {
                               <div className="bg-white p-4 rounded-md shadow-md" key={index}>
                                 <div
                                   className="flex items-start mb-3 ">
-                                  <img onClick={() => Navigate('/profile', { state: post?.userId?._id })}
+                                  <img onClick={() => Navigate('/DevColabs-Client/profile', { state: post?.userId?._id })}
                                     src={post?.userId?.profileImg} alt="User Profile" className="w-10 h-10 rounded-full mr-3 cursor-pointer" />
 
                                   <div>
@@ -525,7 +525,7 @@ function HomePage() {
 
                                     </div>
                                     <p
-                                      onClick={() => Navigate('/profile', { state: post?.userId?._id })}
+                                      onClick={() => Navigate('/DevColabs-Client/profile', { state: post?.userId?._id })}
                                       className="font-medium cursor-pointer text-base sm:text-base overflow-hidden whitespace-wrap break-words"
                                     >
                                       {post?.userId?.UserName}
@@ -714,14 +714,14 @@ function HomePage() {
                                 <div className="flex flex-col">
                                   <div className="ml-4">
                                     <p
-                                      onClick={() => Navigate('/UserPostsView', { state: { UserPost: post } })}
+                                      onClick={() => Navigate('/DevColabs-Client/UserPostsView', { state: { UserPost: post } })}
                                       className="text-lg cursor-pointer overflow-hidden whitespace-wrap break-words"
                                     >
                                       {post?.title}
                                     </p>
                                   </div>
                                   <div className="mt-2 flex-1 border-2">
-                                    <img onClick={() => Navigate('/UserPostsView', { state: { UserPost: post } })}
+                                    <img onClick={() => Navigate('/DevColabs-Client/UserPostsView', { state: { UserPost: post } })}
                                       src={post?.image}
                                       alt={post?.Image}
                                       className="w-full cursor-pointer h-auto"
@@ -790,7 +790,7 @@ function HomePage() {
                                               </button>
                                               <button
                                                 className="ml-3 bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-lg focus:outline-none focus:ring focus:ring-blue-300"
-                                                onClick={() => Navigate('/login')}
+                                                onClick={() => Navigate('/DevColabs-Client/login')}
                                               >
                                                 Login
                                               </button>
@@ -926,14 +926,14 @@ function HomePage() {
                                           {HomePosts[index]?.Comments.map((comment: Comment, commentIndex: number) => (
                                             <div key={ commentIndex} className="mb-2 bg-white ">
                                               <div className="flex items-start p-3 border border-gray-300 rounded-lg hover:shadow-md">
-                                                <img onClick={() => Navigate('/profile', { state: comment?.userId?._id })}
+                                                <img onClick={() => Navigate('DevColabs-Client//profile', { state: comment?.userId?._id })}
                                                   src={comment.userId.profileImg}
                                                   alt="Commenter Profile"
                                                   className="w-10 h-10 rounded-full mr-3  cursor-pointer"
                                                 />
                                                 <div
                                                   className="flex-grow">
-                                                  <p onClick={() => Navigate('/profile', { state: comment?.userId?._id })}
+                                                  <p onClick={() => Navigate('/DevColabs-Client/profile', { state: comment?.userId?._id })}
                                                     className="font-semibold cursor-pointer text-blue-500 hover:underline">
                                                     {comment.userId.UserName}
                                                   </p>
@@ -1048,16 +1048,16 @@ function HomePage() {
                         <nav className="flex flex-col top-44 relative bg-white mr-3 border-2 p-2 pr-2 justify-around rounded-lg shadow-lg">
                           <ul>
                             <li onClick={() => { setSelectCategory('Latest'), setClickedHashtag('') }} className={`flex cursor-pointer items-center w-auto  h-12 space-x-2 ${(!clickedHashtag && selectCategory === 'Latest' || selectCategory === 'Recommended') && `bg-sky-200`}  rounded-xl hover:bg-sky-100 `}>
-                              <AiOutlineHome className="text-3xl text-gray-800  ml-3 " onClick={() => Navigate('/')} />
+                              <AiOutlineHome className="text-3xl text-gray-800  ml-3 " onClick={() => Navigate('/DevColabs-Client/')} />
                               <h1 onClick={() => { setSelectCategory('Latest'), setClickedHashtag('') }} className="font-bold text-base">Home</h1>
                             </li>
-                            <li onClick={() => { username ? Navigate('/Community') : setIsModalOpen(true) }} className="flex cursor-pointer items-center h-12 space-x-2 hover:bg-sky-100 rounded-xl">
+                            <li onClick={() => { username ? Navigate('/DevColabs-Client/Community') : setIsModalOpen(true) }} className="flex cursor-pointer items-center h-12 space-x-2 hover:bg-sky-100 rounded-xl">
                               <HiOutlineUserGroup className="text-3xl text-gray-800 ml-3 mr-1" />
                               <h1 className="font-bold text-base">Community</h1>
                             </li>
                             <li className="flex cursor-pointer items-center h-12 space-x-2 hover:bg-sky-100 rounded-xl">
-                              <AiOutlineUser className="text-3xl text-gray-800 ml-3" onClick={() => Navigate('/profile')} />
-                              <h1 onClick={() => Navigate('/profile')} className="font-bold text-base">Profile</h1>
+                              <AiOutlineUser className="text-3xl text-gray-800 ml-3" onClick={() => Navigate('/DevColabs-Client/profile')} />
+                              <h1 onClick={() => Navigate('/DevColabs-Client/profile')} className="font-bold text-base">Profile</h1>
                             </li>
                           </ul>
                         </nav>
@@ -1092,198 +1092,3 @@ export default HomePage
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <footer className="bg-gray-800 text-white p-4">
-    <div className="container mx-auto text-center">
-      © 2023 Your Brand. All rights reserved.
-    </div>
-  </footer> */}
-
-
-
-
-
-
-
-// <div className="bg-gray-700 left-2 top-28 w-10 h-8 justify-start bottom-2 absolute">
-//                       <div className="flex justify-start relative items-end">
-//                         <BiLike className='text-2xl' />
-//                       </div>
-//                     </div>
-
-
-
-
-
-
-// {/*
-//         <div className="w-screen  flex relative  rounded-sm ">
-//           <div className="relative  h-screen grid grid-cols-1 md:grid-cols-2 m-auto  overflow-y-auto  overflow-x-hidden bottom-1   shadow-md shadow-gray-600 w-[904px]  ">
-
-//             <div className=" z-0  relative items-center mt-24 justify-center  ">
-
-//               {HomePosts && HomePosts.map((post: any, index) => (
-//                 <div key={index} className='flex p-2   relative '>
-//                   <div className="flex relative    ">
-//                     <div className='flex bg-white  relative left-2 rounded-lg m-auto h-[150px]  overflow-hidden shadow-sm shadow-black sm:max-w-[100%]'
-//                       style={{
-//                         width: '54.5rem',
-//                         position: 'relative',
-
-//                       }}
-//                     >
-
-//                       <div onClick={() => Navigate('/profile', { state: post?.userId?._id })} className='z-10 text-start pl-3 pt-3 justify-start  absolute'>
-//                         <img onClick={() => Navigate('/profile', { state: post?.userId })}
-//                           src={post?.userId?.profileImg} alt='User Profile' className='w-9 inline cursor-pointer rounded-full mx-auto ' />
-//                         <h1 onClick={() => Navigate('/profile', { state: post?.userId?._id })}
-//                           className='inline-block pl-1 top-3 text-lg cursor-pointer absolute'>{post?.userId?.UserName}</h1>
-//                         <p className='inline top-4 text-sm left-2 relative'>{post?.Date}</p>
-//                       </div>
-
-//                       <div className='p-4 flex right-0 relative cursor-pointer flex-col justify-center'>
-//                         <h1 onClick={() => Navigate('/UserPostsView', { state: { UserPost: post } })}
-//                           className='font-semibold top-0 left-10  relative text-2xl'>{post?.title} </h1>
-//                         <hr className="border-t border-gray-300 my-4" />
-//                       </div>
-
-//                       <div className=" left-14 top-28 w-52 h-8 justify-start bottom-2 absolute">
-//                         <div className="flex justify-start relative items-end">
-//                           {post.likes.LikedUsers.some((likedUser: any) => likedUser.userId === userId && likedUser.liked) ? (
-//                             <>
-//                               <button onClick={() => handleClick(post._id)} className="hover:bg-gray-300  w-20 justify-center flex rounded-sm">
-
-//                                 <BiSolidLike className='text-2xl opacity-80 cursor-pointer' />
-//                                 <p>Like</p>
-//                                 <span className="ml-1">{post.likes.Count}</span>
-//                               </button>
-//                             </>
-//                           ) : (
-
-//                             <>
-//                               <button onClick={() => handleClick(post._id)} className="hover:bg-gray-300  w-20 justify-center flex rounded-sm">
-
-//                                 <BiLike className='text-2xl opacity-80 cursor-pointer' />
-//                                 <p>Like</p>
-//                                 <span className="ml-1">{post.likes.Count}</span>
-//                               </button>
-
-//                             </>
-//                           )}
-//                           <button onClick={showShadowDiv} className="hover:bg-gray-300 flex left-1 relative rounded-sm">
-//                             <FaRegComments className='text-2xl opacity-80  relative cursor-pointer' />
-//                             <span className=" inline-block relative">Comment</span>
-//                           </button>
-//                         </div>
-//                       </div>
-//                     </div>
-
-
-
-
-
-//                     <div className='flex justify-end  right-8 items-end   relative'>
-//                       <button onClick={() => SavePost(post._id)} >
-//                         <svg className="w-4 right-0 bottom-2 relative" width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                           <path d="M1.9375 0H29.0625C29.5764 0 30.0692 0.158035 30.4325 0.43934C30.7959 0.720644 31 1.10218 31 1.5V30.214C31.0004 30.3483 30.9542 30.4801 30.8662 30.5959C30.7783 30.7116 30.6519 30.8069 30.5001 30.8719C30.3483 30.9369 30.1768 30.9691 30.0035 30.9653C29.8301 30.9614 29.6613 30.9216 29.5146 30.85L15.5 24.046L1.48283 30.848C1.33637 30.919 1.16803 30.9584 0.995292 30.962C0.82255 30.9657 0.651686 30.9335 0.500415 30.8689C0.349143 30.8042 0.222972 30.7094 0.134983 30.5942C0.0469929 30.4791 0.000388072 30.3478 0 30.214V1.5C0 1.10218 0.204129 0.720644 0.56748 0.43934C0.930832 0.158035 1.42364 0 1.9375 0ZM27.125 3H3.875V26.148L15.5 20.508L27.125 26.148V3Z" fill="#5F5454" />
-//                         </svg>
-//                       </button>
-//                     </div>
-//                     <div className="bg-transparent  right-0 left-0  absolute top-36 w-[98.5%]  h-auto">
-//                       <div className="bg-white h-auto relative p-1 left-2 ">
-
-
-
-
-
-//                         <form onSubmit={(e) => {
-//                           e.preventDefault();
-//                           SubmitComments(post._id)
-//                         }}>
-//                           <div className="w-full mb-4 border border-gray-200 rounded-lg h-auto bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-//                             <div className="px-1 py-2 pl-10 bg-white rounded-t-lg h-auto dark:bg-gray-800">
-//                               <div key='' className="flex right-10 top-5 relative items-center">
-//                                 <img src={post?.userId?.profileImg} alt='Commenter Profile' className='w-6 h-6 rounded-full mx-2' />
-//                               </div>
-//                               <label form="comment" className="sr-only">Your comment</label>
-
-//                               <textarea
-//                                 onChange={(e) => SetComment(e.target.value)}
-//                                 id="comment"
-//                                 className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-100 focus:ring-0 dark:text-gray-950 dark:placeholder-gray-400"
-//                                 placeholder="Write a comment..."
-//                                 required
-//                                 rows={3}
-//                               ></textarea>
-//                             </div>
-//                             <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
-//                               <button
-//                                 type="submit"
-//                                 className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
-//                               >
-//                                 Post comment
-//                               </button>
-//                               <div className="flex pl-0 space-x-1 sm:pl-2">
-//                                 <button
-//                                   type="button"
-//                                   className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-//                                 >
-//                                   <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 12 20">
-
-//                                   </svg>
-//                                   <span className="sr-only">Attach file</span>
-//                                 </button>
-
-//                               </div>
-//                             </div>
-//                           </div>
-//                         </form>
-
-
-
-
-
-
-
-//                       </div>
-
-
-//                     </div>
-//                   </div>
-//                 </div>
-
-
-
-
-//               ))}
-//               <ToastContainer />
-
-
-//             </div>
-
-
-//           </div>
-//         </div> */}
--- src/Components/User/Navbar/Navbar.tsx
+++ src/Components/User/Navbar/Navbar.tsx
@@ -221,7 +221,7 @@ function Navbar() {
     googleLogout();
     localStorage.removeItem("user");
     localStorage.removeItem("userId");
-    Navigate("/login");
+    Navigate("/DevColabs-Client/login");
   };
 
   const searchButton = () => {
@@ -257,7 +257,7 @@ function Navbar() {
     event.preventDefault();
     localStorage.setItem('searchTerm', searchTerm);
     try {
-      Navigate('/search-results', { state: { searchTerm } });
+      Navigate('/DevColabs-Client/search-results', { state: { searchTerm } });
       setSearchTerm(searchTerm)
     } catch (error) {
 
@@ -331,7 +331,7 @@ function Navbar() {
                         <ul className="list-none">
                           <li className="border-b border-gray-400">
                             <a
-                              onClick={() => { Navigate("/profile"), RemoveSearchTerm() }}
+                              onClick={() => { Navigate("/DevColabs-Client/profile"), RemoveSearchTerm() }}
                               className="block px-4 py-2 text-gray-700 dark:hover:bg-gray-600 dark:hover:text-white font-medium"
                             >
                               Profile
@@ -340,7 +340,7 @@ function Navbar() {
                           <li className="border-b border-gray-400">
                             <button
                               onClick={() => {
-                                Navigate("/SavedPosts"), RemoveSearchTerm();
+                                Navigate("/DevColabs-Client/SavedPosts"), RemoveSearchTerm();
                               }}
                               className="flex items-center px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-900 dark:hover:text-white"
                             >
@@ -408,7 +408,7 @@ function Navbar() {
               ) : (
                 <div className="lg:hidden  md:hidden">
                   <button
-                    onClick={() => { Navigate("/login"), RemoveSearchTerm() }}
+                    onClick={() => { Navigate("/DevColabs-Client/login"), RemoveSearchTerm() }}
                     className="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-3"
                   >
                     Log in
@@ -566,7 +566,7 @@ function Navbar() {
                   <li className="mx-4 items-center flex flex-col justify-center relative my-6 md:my-0">
                     <button
                       onClick={() => {
-                        Navigate("/PostCreation"), RemoveSearchTerm();
+                        Navigate("/DevColabs-Client/PostCreation"), RemoveSearchTerm();
                       }}
                       className="text-xl  hover:text-cyan-500 duration-500"
                     >
@@ -623,14 +623,14 @@ function Navbar() {
                                   </div>
                                   <div className="flex-grow">
                                     {notfication?.ReportPostId ? (<>
-                                      <p className="text-gray-800" onClick={() => { Navigate('/UserPostsView', { state: { UserPost: notfication?.ReportPostId } }), RemoveSearchTerm() }} >{notfication?.ReportPostId?.title}</p>
+                                      <p className="text-gray-800" onClick={() => { Navigate('/DevColabs-Client/UserPostsView', { state: { UserPost: notfication?.ReportPostId } }), RemoveSearchTerm() }} >{notfication?.ReportPostId?.title}</p>
                                       <div className="flex justify-between items-center">
                                         <p className="text-xs text-gray-500">{notfication?.NotifyDate}</p>
-                                        <p className="text-sm font-serif text-rose-800" onClick={() => { Navigate("/profile"), RemoveSearchTerm() }}>{notfication?.Message}</p>
+                                        <p className="text-sm font-serif text-rose-800" onClick={() => { Navigate("/DevColabs-Client/profile"), RemoveSearchTerm() }}>{notfication?.Message}</p>
                                       </div></>
                                     ) : (<>
                                       <div className="flex justify-between">
-                                        <p className="text-gray-800 inline-block" onClick={() => { Navigate('/Community', { state: { chat: notfication?.senderId?._id } }), RemoveSearchTerm() }} >{notfication?.senderId?._id !== userId ? notfication?.senderId?.UserName : notfication?.userId?.UserName}</p>
+                                        <p className="text-gray-800 inline-block" onClick={() => { Navigate('/DevColabs-Client/Community', { state: { chat: notfication?.senderId?._id } }), RemoveSearchTerm() }} >{notfication?.senderId?._id !== userId ? notfication?.senderId?.UserName : notfication?.userId?.UserName}</p>
                                         <p className="text-xs text-gray-500 inline-block">
                                           {notfication?.ChatMessage?.timestamp &&
                                             new Date(notfication?.ChatMessage?.timestamp).toLocaleTimeString([], {
@@ -642,7 +642,7 @@ function Navbar() {
                                       </div>
                                       <div className="flex justify-between items-center">
                                         <p className="text-xs text-gray-500 bg-gray-200 rounded-lg p-1">{notfication?.ChatMessage?.text}</p>
-                                        <p className="text-sm font-serif text-rose-800" onClick={() => { Navigate("/profile"), RemoveSearchTerm() }}>{notfication?.Message}</p>
+                                        <p className="text-sm font-serif text-rose-800" onClick={() => { Navigate("/DevColabs-Client/profile"), RemoveSearchTerm() }}>{notfication?.Message}</p>
                                       </div></>
                                     )}
                                   </div>
@@ -698,7 +698,7 @@ function Navbar() {
                           </div>
                           <li>
                             <a
-                              onClick={() => { Navigate("/profile"), RemoveSearchTerm() }}
+                              onClick={() => { Navigate("/DevColabs-Client/profile"), RemoveSearchTerm() }}
 
                               className="block px-4 py-2  text-gray-700 dark:hover:bg-gray-600 dark:hover:text-white font-medium"
                             >
@@ -708,7 +708,7 @@ function Navbar() {
                           <li>
                             <button
                               onClick={() => {
-                                Navigate("/SavedPosts"), RemoveSearchTerm()
+                                Navigate("/DevColabs-Client/SavedPosts"), RemoveSearchTerm()
                               }}
                               className="flex items-center px-4 py-2 w-full hover:bg-gray-100 dark:hover:bg-gray-600 hover:text-gray-900 dark:hover:text-white"
                             >
@@ -746,7 +746,7 @@ function Navbar() {
                 </>
               ) : (
                 <button
-                  onClick={() => Navigate("/login")}
+                  onClick={() => Navigate("/DevColabs-Client/login")}
                   className="inline-flex items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ml-3"
                 >
                   Log in
