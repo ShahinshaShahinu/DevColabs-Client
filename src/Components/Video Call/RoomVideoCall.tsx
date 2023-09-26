007c4a14749899583cabbae6484b84c9a9faa36c
Muhammed Shahinsha.c <muhammedshahinsha2442@gmail.com>
2023-09-26 18:48:10 +0530

    ee

--- src/Components/Video Call/RoomVideoCall.tsx
+++ src/Components/Video Call/RoomVideoCall.tsx
@@ -38,7 +38,7 @@ function RoomVideoCall() {
         // Get the current camera stream based on the frontCamera state
         const stream = await navigator.mediaDevices.getUserMedia({
           audio: true,
-          video: { facingMode: 'environment' },
+          video: { facingMode: 'user' },
         });
         console.log(frontCamera,'camera');console.log(frontCamera,'camera');
         // Continue with stream handling
