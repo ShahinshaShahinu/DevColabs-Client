6e82893f029dec127f1814f03f0f62bd065a1eab
ShahinshaShahinu <muhammedshahinsha2442@gmail.com>
2024-05-01 21:29:19 +0530

    r

--- src/Components/User/Home/Home.tsx
+++ src/Components/User/Home/Home.tsx
@@ -952,7 +952,7 @@ function HomePage() {
                                       Add Hashtag
                                     </h3>
                                     <button
-                                      onClick={() => {setSelectHashtag(false),setRefresh(true)}}
+                                      onClick={() => {setSelectHashtag(false),setRefresh(true),setSelectCategory('Latest')}}
                                       type="button" className="text-white bg-gray-700 hover:bg-gray-500 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-700 dark:hover:bg-gray-700focus:outline-none dark:focus:ring-blue-800">
                                       Skip</button>
 
